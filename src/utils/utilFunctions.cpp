b0a056f3a095a856a9e6e808c35cbe32635b8b33
zavgm <zavgmaxden@mail.ru>
2023-05-11 18:22:21 +0300

    wrapped utilities in namepace

--- src/utils/utilFunctions.cpp
+++ src/utils/utilFunctions.cpp
@@ -4,22 +4,24 @@
 
 #include "utilFunctions.h"
 
-bool isWhitespace(char symbol) {
-    static constexpr char whitespaces[] = { ' ', '\t', '\0' };
-    return std::any_of(std::begin(whitespaces), std::end(whitespaces), [symbol](char wp) -> bool {
-        return symbol == wp;
-    });
-}
+namespace MonUt {
+    bool isWhitespace(char symbol) {
+        static constexpr char whitespaces[] = {' ', '\t', '\0'};
+        return std::any_of(std::begin(whitespaces), std::end(whitespaces), [symbol](char wp) -> bool {
+            return symbol == wp;
+        });
+    }
 
-void toLower(std::string &text) {
-    std::for_each(std::begin(text), std::end(text), [](char &symbol) -> void{
-        symbol = static_cast<char>(std::tolower(symbol));
-    }); // cast to lower case
-}
+    void toLower(std::string &text) {
+        std::for_each(std::begin(text), std::end(text), [](char &symbol) -> void {
+            symbol = static_cast<char>(std::tolower(symbol));
+        }); // cast to lower case
+    }
 
-std::string toLower(std::string &&text) {
-    std::for_each(std::begin(text), std::end(text), [](char &symbol) -> void{
-        symbol = static_cast<char>(std::tolower(symbol));
-    }); // cast to lower case
-    return text;
+    std::string toLower(std::string &&text) {
+        std::for_each(std::begin(text), std::end(text), [](char &symbol) -> void {
+            symbol = static_cast<char>(std::tolower(symbol));
+        }); // cast to lower case
+        return text;
+    }
 }
