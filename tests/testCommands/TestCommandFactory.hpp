6d80f2cf6fb5153a711da585834021ebd6e2c322
zavgm <zavgmaxden@mail.ru>
2023-05-14 21:42:27 +0300

    fixed test

--- tests/testCommands/TestCommandFactory.hpp
+++ tests/testCommands/TestCommandFactory.hpp
@@ -8,6 +8,13 @@
 #include "Init.h"
 #include "Help.h"
 
-class TestCommandFactory : public CommandFactory<std::tuple<Init, Help>> {};
+class TestCommandFactory : public CommandFactory<std::tuple<Init, Help>> {
+public:
+    explicit TestCommandFactory() = default;
+
+    std::shared_ptr<BaseCommand> getCommand(const std::string &commandString) override {
+        return construct(commandString);
+    }
+};
 
 #endif //MONITOR_TESTCOMMANDFACTORY_HPP
